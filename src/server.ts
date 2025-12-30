203282b18412d29d29bed42baa6dda7311bf95e2
knaeeim <mdkhairulb01@gmail.com>
2025-12-31 03:52:25 +0600

    Prisma SetUp Done with required changes in package.json and tsconfig.ts files

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import app from "./app";
+
+const PORT = process.env.PORT || 3000;
+
+async function run() {
+    app.listen(PORT, () => {
+        console.log(`Server is running on http://localhost:${PORT}`);
+    })
+}
+
+run();
