f198358ac4e6c6ae7ce19903bfb259206caeb879
knaeeim <mdkhairulb01@gmail.com>
2025-12-31 18:10:09 +0600

    getEquipments controller defined and working fine

--- src/modules/equipment/equipment.controller.ts
+++ src/modules/equipment/equipment.controller.ts
@@ -20,7 +20,23 @@ const createEquipment = async (req: Request, res: Response) => {
     }
 }
 
+const getEquipment = async (req: Request, res: Response) => {
+    try {
+        const equipments = await prisma.equipment.findMany(); 
+        res.status(200).json({
+            message: "Equipments retrieved successfully",
+            data: equipments
+        })
+    } catch (error : any) {
+        res.status(500).json({
+            message: "Internal server error",
+            error: error.message
+        })
+    }
+}
+
 
 export const equipmentController = {
-    createEquipment
+    createEquipment, 
+    getEquipment,
 }
--- src/modules/equipment/equipment.routes.ts
+++ src/modules/equipment/equipment.routes.ts
@@ -3,6 +3,8 @@ import { equipmentController } from "./equipment.controller";
 
 const router = Router(); 
 
-router.post('/', equipmentController.createEquipment)
+router.post('/', equipmentController.createEquipment);
+
+router.get('/', equipmentController.getEquipment);
 
 export const equipmentRoutes = router;
