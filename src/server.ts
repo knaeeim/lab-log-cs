af1f0f118dde5bedf04e08d867421fa84cce36ad
knaeeim <mdkhairulb01@gmail.com>
2025-12-31 11:39:43 +0600

    Prisma Client Set Perfectly, Path Changed of generate folder due to all I need to do in src folder and in server.ts file connected the prisma adapter successfully

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,20 @@
 import app from "./app";
+import { prisma } from "./lib/prisma";
 
 const PORT = process.env.PORT || 3000;
 
 async function run() {
-    app.listen(PORT, () => {
-        console.log(`Server is running on http://localhost:${PORT}`);
-    })
+    try {
+        await prisma.$connect();
+        console.log("Database Connected Successfully!");
+        
+        app.listen(PORT, () => {
+            console.log(`Server is running on http://localhost:${PORT}`);
+        })
+    } catch (error: any) {
+        console.log(error.message);
+        await prisma.$disconnect();
+    }
 }
 
 run();
